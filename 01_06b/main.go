fbf3d9f7f654fa74d36b84a33a20403051d96b60
oscar.sanguino <oscar.sanguino@contractor.vroom.com>
2023-02-22 16:37:51 -0500

    solving challenge 6

---
+++ 01_06b/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+// User represents a user record.
+type User struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+const path = "users.json"
+
+// getBiggestMarket takes in the slice of users and
+// returns the biggest market.
+func getBiggestMarket(users []User) (string, int) {
+	countryUsers := make(map[string]int)
+
+	for _, user := range users{
+		countryUsers[user.Country]++
+	}
+
+	var maxUserCountryAmount int
+	var biggestUsersCountry string
+
+	for country, amount := range countryUsers {
+		if amount > maxUserCountryAmount {
+			maxUserCountryAmount = amount
+			biggestUsersCountry = country
+		}
+	}
+
+	return biggestUsersCountry, maxUserCountryAmount
+}
+
+func main() {
+	users := importData()
+	country, count := getBiggestMarket(users)
+	log.Printf("The biggest user market is %s with %d users.\n",
+		country, count)
+}
+
+// importData reads the raffle entries from file and
+// creates the entries slice.
+func importData() []User {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data []User
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
